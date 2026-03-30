26ae9bdfe8d717a9089f860e53d4ec99dbda8381
wzxcff <Shevasfloyd@gmail.com>
2026-03-31 00:53:31 +0200

    feat: add seed for transparancy of shuffling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import json
+import datetime
 
 
 def get_student_names() -> list[dict]:
@@ -26,6 +27,14 @@ def print_students(students: list[dict], start_from: int = 0) -> None:
 
 
 def main():
+    seed = input("Set seed if you want to, leave blank to skip: ")
+    if not seed:
+        seed = datetime.date.today().strftime("%Y%m%d")
+
+    seed = int(seed)
+
+    print(f"Shuffling student with the current seed: {seed}")
+
     priority = form_priority(get_student_names())
 
     print("\nStudents with debt (PRIORITY):")
